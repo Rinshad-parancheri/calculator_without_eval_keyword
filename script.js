19ce36a5b34658c80beaba3e393d103cc7d0d4d3
rinshadpdev <rinshadp014@gmail.com>
2023-11-09 07:10:34 +0530

    add dom manipulation append number to display , functionality to the clear and delete btns

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//dom elements
+const display = document.getElementById("display");
+const equalBtn =  document.getElementById('equalsBtn');
+const btns = document.querySelectorAll(".btns");
+const deleteBtn = document.getElementById('deleteBtn');
+const clearBtn = document.getElementById('clearBtn');
+
+//Event listeners;
+deleteBtn.addEventListener('click', (e) =>{
+  display.value = display.value.slice(0, -1);
+})
+
+btns.forEach(e => {
+    e.addEventListener('click', (e) => {
+        display.value += e.target.innerText
+    })
+ })
+
+ clearBtn.onclick = (e) => display.value = '';
+
+
 function operate(num1, num2, operator) {
     switch (operator) {
         case '+':
@@ -26,4 +47,4 @@ function multiply(num1, num2) {
 }
 function division(num1, num2) {
     return num1 / num2;
-}
+}    
