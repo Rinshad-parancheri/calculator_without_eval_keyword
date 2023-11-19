d0cc90e1e09b40dc9adf902edfcc54f2d3a521ba
rinshadpdev <rinshadp014@gmail.com>
2023-11-19 12:08:49 +0530

    add logic to multiple opertion

--- script.js
+++ script.js
@@ -1,50 +1,158 @@
-//dom elements
-const display = document.getElementById("display");
-const equalBtn =  document.getElementById('equalsBtn');
-const btns = document.querySelectorAll(".btns");
-const deleteBtn = document.getElementById('deleteBtn');
-const clearBtn = document.getElementById('clearBtn');
+class Display {
+    constructor() {
+        this.displayElement = document.getElementById('display');
+        this.resultDisplay = document.getElementById('resultDisplay');
+        console.log(this.resultDisplay);
+    }
 
-//Event listeners;
-deleteBtn.addEventListener('click', (e) =>{
-  display.value = display.value.slice(0, -1);
-})
+    clear() {
+        this.displayElement.innerText = '';
+        this.resultDisplay.innerText = '';
+    }
 
-btns.forEach(e => {
-    e.addEventListener('click', (e) => {
-        display.value += e.target.innerText
-    })
- })
+    update(value) {
+        if (this.displayElement.innerText == '0') {
+            this.displayElement.innerText = value;
 
- clearBtn.onclick = (e) => display.value = '';
+        } else {
+            this.displayElement.innerText += value;
+        }
+    }
 
+    updateOperator (e) {
+        if(this.displayElement.innerText !== '0'){
+            this.displayElement.innerText += ` ${e} `
+        }
+    }
 
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '/':
-            multiply(num1, num2);
-            break;
-        case '*':
-            division(num1, num2);
-            break;
+    deleteLastCharacter() {
+        const text = this.displayElement.innerText;
+        this.displayElement.innerText = text.slice(0, -1);
     }
 }
 
-function add(num1, num2) {
-    return num1 + num2;
+
+class Calculator extends Display {
+    constructor(operation) {
+        super();
+        this.operattions = operation;
+        this.firstNum = '';
+        this.secondNum = '';
+        this.operator = '';
+    }
+
+    handleClear() {
+        this.firstNum = '';
+        this.secondNum = '';
+        this.operator = '';
+        this.clear();
+    }
+
+    handleDelete() {
+        this.deleteLastCharacter();
+    }
+
+    handleNumber(number) {
+        this.update(number);
+
+        if (this.operator === '') {
+            this.firstNum += number;
+        } else if (this.operator !== ''){
+            this.secondNum += number;
+            this.resultDisplay.innerText = this.operattions[this.operator](Number(this.firstNum),Number(this.secondNum));
+            this.firstNum = this.resultDisplay.innerText;
+            this.operator = '';
+            this.secondNum = ''; 
+        }
+
+    }
+
+    handleOperatorBtn(e) {
+        this.updateOperator(e)
+        switch (e) {
+            case '+':
+                this.operator = 'add';
+                break; ``
+            case '-':
+                this.operator = 'subtraction';
+                break;
+            case 'x':
+                this.operator = 'multiplication';
+                break;
+            case '÷':
+                this.operator = 'division';
+            break;
+        }
+    }
 }
-function subtract(num1, num2) {
-    return num1 - num2;
+
+
+
+
+
+
+class Operations {
+    add(num1, num2) {
+        return num1 + num2;
+    }
+
+    division(num1, num2) {
+        return num1 / num2;
+    }
+
+    subtraction(num1, num2) {
+        return num1 - num2;
+    }
+
+    multiplication(num1, num2) {
+        return num1 * num2;
+    }
 }
-function multiply(num1, num2) {
-    return num1 * num2;
+
+
+class CalculatorUI {
+    constructor(calculator) {
+        this.calculator = calculator;
+        this.numberButtons = document.querySelectorAll('.btns');
+        this.operatorButtons = document.querySelectorAll('.operator');
+        this.deleteButton = document.getElementById('deleteBtn');
+        this.clearButton = document.getElementById('clearBtn');
+
+        this.attachEventListeners();
+    }
+
+    attachEventListeners() {
+        this.NumberButtonClicks();
+        this.OperatorButtonClicks();
+        this.ClearButtonClick();
+        this.DeleteButtonClick();
+    }
+
+    NumberButtonClicks() {
+        this.numberButtons.forEach(btn => {
+            btn.onclick = (e) => this.calculator.handleNumber(e.target.innerText);
+        })
+    }
+
+    OperatorButtonClicks() {
+        this.operatorButtons.forEach(operatorBtn => {
+        operatorBtn.onclick = (e) = this.calculator.handleOperatorBtn(e.target);
+    })
+    
 }
-function division(num1, num2) {
-    return num1 / num2;
-}    
+    ClearButtonClick() {
+        this.clearButton.addEventListener('click', () => {
+            this.calculator.handleClear();
+        });
+    }
+
+    DeleteButtonClick() {
+        this.deleteButton.addEventListener('click', () => {
+            this.calculator.handleDelete();
+        });
+    }
+}
+
+
+const calculator = new Calculator(new Operations);
+const calculatorUI = new CalculatorUI(calculator);
